4e0b2f151344b4d192370d0bd7e712ea90d8e124
matix130 <mateusz.kedra@poczta.onet.pl>
2018-12-17 20:28:45 +0100

    django

---
+++ web_apps/examples/maths/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Math (models.Model):
+
+    operacja = models.CharField(max_length=4)
+    a= models.IntegerField()
+    b = models.IntegerField()
+    wynik = models.TextField()
---
+++ web_apps/examples/maths/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+from maths.models import Math
+
+
+def obliczenia(request, dzialanie, zmienna1, zmienna2):
+    z1=int(zmienna1)
+    z2=int(zmienna2)
+
+    if dzialanie=="dodawanie":
+        wynik=z1+z2
+    elif dzialanie=="odejmowanie":
+        wynik=z1-z2
+    elif dzialanie=="mnozenie":
+        wynik=z1*z2
+    elif dzialanie=="dzielenie":
+        if z2==0:
+            return HttpResponse("Nie dziel cholero przez zero!")
+        else:
+            wynik=z1/z2
+
+    m = Math(działanie=dzialanie, a=z1, b=z2, wynik=wynik)
+    m.save()
+    return HttpResponse(f"Twój wynik to {wynik}")
